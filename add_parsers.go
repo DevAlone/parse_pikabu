d3f56c15757b87312b473a884bef7c62f23441f4
d3dev <admin@d3d.info>
2019-01-09 18:22:30 +0000

    script for adding parsers

--- add_parsers.go
+++ add_parsers.go
@@ -6,22 +6,29 @@ import (
 	"strings"
 
 	"bitbucket.org/d3dev/parse_pikabu/helpers"
+	"bitbucket.org/d3dev/parse_pikabu/parser"
 	"bitbucket.org/d3dev/parse_pikabu/server/middlewares"
 	"github.com/go-errors/errors"
 )
 
 func addParsers() error {
 	redisClient := helpers.GetRedisClient()
-	// parse_pikabu_server_authentication_middleware_session_group_
-	//
 	if len(os.Args) < 2 {
 		handleError(errors.New("too few arguments"))
 	}
-	key := "parse_pikabu_server_authentication_middleware_session_group_" + strings.TrimSpace(os.Args[1])
-	err := redisClient.Set(key, fmt.Sprint(middlewares.GROUP_PARSER), 0).Err()
-
+	parsersConfig, err := parser.NewParsersConfigFromFile(os.Args[1])
 	if err != nil {
 		return err
+	}
+
+	for _, parserConfig := range parsersConfig.Configs {
+		sessionId := strings.TrimSpace(parserConfig.ApiSessionId)
+		key := "parse_pikabu_server_authentication_middleware_session_group_" + sessionId
+		err := redisClient.Set(key, fmt.Sprint(middlewares.GROUP_PARSER), 0).Err()
+
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
