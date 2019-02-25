d8087b872bcebd0e42c2b6d1b6be48ad268ad5c7
d3dev <admin@d3d.info>
2019-02-25 11:32:10 +0000

    fix

--- tests/helpers.go
+++ tests/helpers.go
@@ -1,28 +1,7 @@
 package tests
 
-import (
-	"os"
-
-	"bitbucket.org/d3dev/parse_pikabu/core/config"
-	"bitbucket.org/d3dev/parse_pikabu/core/logger"
-	"bitbucket.org/d3dev/parse_pikabu/helpers"
-	logging "github.com/op/go-logging"
-)
+import "bitbucket.org/d3dev/parse_pikabu/core/logger"
 
 func initLogs() {
-	file, err := os.OpenFile("../logs/tests.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		helpers.PanicOnError(err)
-	}
-	loggingBackend := logging.NewLogBackend(file, "", 0)
-	loggingBackendFormatter := logging.NewBackendFormatter(loggingBackend, logger.LogFormat)
-
-	logging.SetBackend(loggingBackend, loggingBackendFormatter)
-
-	if config.Settings.Debug {
-		logging.SetLevel(logging.DEBUG, "parse_pikabu")
-	} else {
-		logging.SetLevel(logging.WARNING, "parse_pikabu")
-	}
-
+	logger.Init()
 }
