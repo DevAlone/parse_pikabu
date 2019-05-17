addfc8bf5317d93f58c6b1690948e7284cfb52c1
d3dev <admin@d3d.info>
2019-05-17 22:35:14 +0000

    fix

--- core/core.go
+++ core/core.go
@@ -64,6 +64,18 @@ func Main() {
 		wg.Done()
 	}()
 
+	modelhooks.HandlePikabuCommentChange(
+		models.PikabuComment{
+			Text:      "test1",
+			IsDeleted: false,
+		},
+		models.PikabuComment{
+			Text:      "test2",
+			IsDeleted: true,
+		},
+		models.TimestampType(0),
+	)
+
 	wg.Wait()
 
 	logger.Cleanup()
