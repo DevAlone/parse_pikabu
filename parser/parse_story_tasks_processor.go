9ffbf41d0ead32b4dd2116fdf3ed876a323e0a18
d3dev <admin@d3d.info>
2019-08-17 18:12:14 +0000

    fix bug

--- parser/parse_story_tasks_processor.go
+++ parser/parse_story_tasks_processor.go
@@ -22,7 +22,9 @@ func (p *Parser) processParseStoryTask(task *models.ParseStoryTask) error {
 			})
 		}
 		if pe, ok := err.(*pikago.PikabuError); ok {
-			if strings.Contains(pe.Message, "Google Play Market") {
+			// Пост содержит материалы, запрещенные политикой магазина приложений, вы можете посмотреть его в веб-версии Пикабу
+			if strings.Contains(pe.Message, "Google Play Market") ||
+				strings.Contains(pe.Message, "\u0437\u0430\u043f\u0440\u0435\u0449\u0435\u043d\u043d\u044b\u0435 \u043f\u043e\u043b\u0438\u0442\u0438\u043a\u043e\u0439 \u043c\u0430\u0433\u0430\u0437\u0438\u043d\u0430 \u043f\u0440\u0438\u043b\u043e\u0436\u0435\u043d\u0438\u0439") {
 				// consider as deleted
 				return p.PutResultsToQueue("story_not_found", []models.ParserStoryNotFoundResultData{
 					{
