1d1272e66a45d80e52fe44d5fa56a82c4c7d39e7
d3dev <admin@d3d.info>
2020-02-07 21:42:26 +0000

    fix

--- core/taskmanager/user_tasks.go
+++ core/taskmanager/user_tasks.go
@@ -165,7 +165,8 @@ func updateUsersWorker() error {
 				time.Now().Unix(),
 				time.Now().Unix()-int64(config.Settings.MaximumParseUserTaskProcessingTime),
 			).
-			Order("next_update_timestamp").
+			// Order("next_update_timestamp").
+			Order("random()").
 			Limit(config.Settings.GetItemsToUpdateAtTime).
 			Select()
 		if err != pg.ErrNoRows && err != nil {
